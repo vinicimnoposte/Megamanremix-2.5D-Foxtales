5fc36571e39bf7d9cfee275c1ada18e1ee64681c
vinicimnoposte <64757351+vinicimnoposte@users.noreply.github.com>
2021-03-10 15:10:08 -0300

    Initial commit

---
+++ Assets/Scripts/Morte.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Morte : MonoBehaviour
+{
+    [SerializeField]
+    private Transform respawn;
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.CompareTag("Player")) 
+            col.transform.position = respawn.position;
+    }
+
+}
